7861a9d0f198fec934f4cf81deafa7aa3ffb72c6
Alberto Corona <albcoron@gmail.com>
2014-03-21 22:43:29 -0500

    Update mkpost to reflect directory changes
    Signed-off-by: Alberto Corona <albcoron@gmail.com>

--- mkpost.sh
+++ mkpost.sh
@@ -4,16 +4,16 @@ DATE=$(date +%Y-%m-%d)
 echo "Enter post filename:"
 read PSTNME
 POST=$DATE-$PSTNME.md
-touch _posts/$DATE-$PSTNME.md
-echo --- > _posts/$POST
-echo "layout: post" >> _posts/$POST
+touch blog/_posts/$DATE-$PSTNME.md
+echo --- > blog/_posts/$POST
+echo "layout: post" >> blog/_posts/$POST
 echo "Enter title:"
 read TTLE
-echo "title: \"$TTLE\"" >> _posts/$POST
-echo "date: $DATE" >> _posts/$POST
+echo "title: \"$TTLE\"" >> blog/_posts/$POST
+echo "date: $DATE" >> blog/_posts/$POST
 echo "Enter tags:"
 read TAGS
-echo "tags: $TAGS" >> _posts/$POST
-echo --- >> _posts/$POST
+echo "tags: $TAGS" >> blog/_posts/$POST
+echo --- >> blog/_posts/$POST
 
-vim _posts/$POST
+vim blog/_posts/$POST
