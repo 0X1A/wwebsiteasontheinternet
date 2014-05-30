4c2dbe498987de7f3ac920ba3e5a7d6f3254612f
Alberto Corona <alberto@0x1a.us>
2014-05-29 23:05:59 -0500

    Fix pkgs directory
    Signed-off-by: Alberto Corona <alberto@0x1a.us>

--- mkpkgs.sh
+++ mkpkgs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cp -v ~/Pkgs/*.pkg.* ~/Repos/wwebsiteasontheinternet/pkgs
-repo-add -s -k 4B2076CA -v ~/Repos/wwebsiteasontheinternet/pkg/0X1A.db.tar.gz ~/Repos/wwebsiteasontheinternet/pkgs/*.pkg.*
+repo-add -s -k 4B2076CA -v ~/Repos/wwebsiteasontheinternet/pkgs/0X1A.db.tar.gz ~/Repos/wwebsiteasontheinternet/pkgs/*.pkg.*
